fc846f8af1ce97f1e75b5c73e618534f80565125
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-07-24 17:25:33 -0300

    Criacao e configuracao do Selenium para automação de testes leilao

---
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,36 @@
+package br.com.alura.leilao;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/* public class selectProduct {
+    WebDriver browser;
+
+    @BeforeAll
+    public void beforeAll(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver
+    }
+    */
+
+}
+
+
+public class HelloWorldSelenium {
+
+
+
+
+    @Test
+    public void hello(){
+        System.setProperty("webdriver.chrome.driver", "/drivers/chromedriver");
+        WebDriver browser = new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        browser.navigate().to("https://localhost:8080/leiloes");
+        browser.quit();
+
+    }
+}

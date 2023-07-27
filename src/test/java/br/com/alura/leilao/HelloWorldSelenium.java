febcfcaa782e8ebe36ae7888db4d3844bb8f16c6
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-07-26 22:48:57 -0300

    Test envio

--- src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-/* public class selectProduct {
+/* public class selectProduct {//
     WebDriver browser;
 
     @BeforeAll

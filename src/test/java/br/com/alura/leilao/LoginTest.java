9586d737431b541bbefaa6e13c5cf7f77975399a
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-08-09 16:23:31 -0300

    Criação código utilizando Selenium com Java

---
+++ src/test/java/br/com/alura/leilao/LoginTest.java
@@ -0,0 +1,63 @@
+package br.com.alura.leilao;
+
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    private static final String URL_LOGIN = "https://localhost:8080/login";
+    private WebDriver browser;
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+    }
+
+
+    @BeforeEach
+    public void beforeEach(){
+        System.setProperty("webdriver.chrome.driver", "/drivers/chromedriver");
+        this.browser = new ChromeDriver();
+        browser.navigate().to(URL_LOGIN);
+    }
+    @AfterEach
+    public void afterEach(){
+        this.browser.quit();
+    }
+    @Test
+    public void deveriaEfetuarLoginComDadosValidos(){
+
+        browser.findElement(By.id("username")).sendKeys("fulano");
+        browser.findElement(By.id("password")).sendKeys("password");
+        browser.findElement(By.id("login-form")).submit();
+
+        Assert.assertFalse(browser.getCurrentUrl().equals(URL_LOGIN));
+        Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+
+    }
+    @Test
+    public void naoDeveriaLogarComDadosInvalidos(){
+
+        browser.findElement(By.id("username")).sendKeys("invalido");
+        browser.findElement(By.id("password")).sendKeys("123123");
+        browser.findElement(By.id("login-form")).submit();
+
+        Assert.assertTrue(browser.getCurrentUrl().equals("https://localhost:8080/login?error"));
+        Assert.assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
+        Assert.assertThrows(NoSuchElementException.class,() -> browser.findElement(By.id("usuario-logado")).getText());
+
+    }
+    @Test
+    public void naoDeveriaAcessarPaginaRestritaSemEstarLogado(){
+        this.browser.navigate().to("https://localhost:8080/leiloes/2");
+        Assert.assertTrue(browser.getCurrentUrl().equals("https://localhost:8080/login"));
+        Assert.assertFalse(browser.getPageSource().contains("Dados do Leilão"));
+    }
+}
